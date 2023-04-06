87cd29b795e32e3952f708fe714ccffc59d0d87e
Toma Ligia <ll943145@gmail.com>
2023-04-06 22:44:15 +0300

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { BrowserRouter } from "react-router-dom";
 import "../styles/App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello from App</h1>
-    </div>
+    // <div className="App">
+    //   <h1>Hello from App</h1>
+    // </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Form/>}></Route>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App/> */}
     {/* <Nav/> */}
-    {/* <Form /> */}
-    <Login/>
+    <Form />
+    {/* <Login/> */}
   </React.StrictMode>,
 )
